117d5aa52fa2285cec0b1a1e3a5cf1c0c37ebeb4
JorgePinetta <jorgepinetta@puertadigital.com>
2024-02-28 05:31:33 -0300

    * release 1.1.2

--- TDAmeritrade.Console/Program.cs
+++ TDAmeritrade.Console/Program.cs
@@ -1,5 +1,4 @@
-﻿using FlatSharp;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
--- TDAmeritrade.Tests/Tests.cs
+++ TDAmeritrade.Tests/Tests.cs
@@ -15,7 +15,7 @@ namespace TDAmeritrade.Tests
             var cache = new TDUnprotectedCache();
             client = new TDAmeritradeClient(cache);
             await client.SignIn();
-            Assert.IsTrue(client.IsSignedIn);
+            Assert.That(client.IsSignedIn);
         }
 
         [Test]
@@ -23,20 +23,20 @@ namespace TDAmeritrade.Tests
         {
             double stamp1 = 1464148800000 / 1000;
             var time1 = TDHelpers.FromUnixTimeSeconds(stamp1);
-            Assert.IsTrue(time1.Minute == 00);
-            Assert.IsTrue(time1.Hour == 4);
-            Assert.IsTrue(time1.Day == 25);
-            Assert.IsTrue(time1.Month == 5);
-            Assert.IsTrue(time1.Year == 2016);
-            Assert.IsTrue(time1.DayOfWeek == System.DayOfWeek.Wednesday);
+            Assert.That(time1.Minute == 00);
+            Assert.That(time1.Hour == 4);
+            Assert.That(time1.Day == 25);
+            Assert.That(time1.Month == 5);
+            Assert.That(time1.Year == 2016);
+            Assert.That(time1.DayOfWeek == System.DayOfWeek.Wednesday);
 
             var time2 = new DateTime(2016, 5, 25, 4, 0, 0, 0, DateTimeKind.Utc);
             var time3 = TDHelpers.ToEST(time2);
-            Assert.IsTrue(time3.Hour == 0);
+            Assert.That(time3.Hour == 0);
 
             var stamp2 = time2.ToUnixTimeSeconds();
             var stamp3 = TDHelpers.UnixSecondsToMiliseconds(stamp2);
-            Assert.IsTrue(stamp3 == 1464148800000);
+            Assert.That(stamp3 == 1464148800000);
         }
 
         [Test]
@@ -46,7 +46,7 @@ namespace TDAmeritrade.Tests
             {
                 symbol = "QQQ",
             });
-            Assert.IsTrue(chain.callExpDateMap.Count > 0);
+            Assert.That(chain.callExpDateMap.Count > 0);
         }
 
 
@@ -54,7 +54,7 @@ namespace TDAmeritrade.Tests
         public async Task TestMarketHours()
         {
             var hours = await client.GetMarketHours(MarketTypes.EQUITY, DateTime.Now);
-            Assert.IsTrue(hours.marketType == "EQUITY");
+            Assert.That(hours.marketType == "EQUITY");
         }
 
 
@@ -67,17 +67,17 @@ namespace TDAmeritrade.Tests
                 array[i] = new TDPriceCandle { close = i, high = i, low = i, open = i, volume = i, datetime = i };
             }
             var merge1 = TDHelpers.ConsolidateByTotalCount(array, 3);
-            Assert.IsTrue(merge1.Length == 3);
+            Assert.That(merge1.Length == 3);
             var merge2 = TDHelpers.ConsolidateByTotalCount(array, 5);
-            Assert.IsTrue(merge2.Length == 5);
+            Assert.That(merge2.Length == 5);
             var merge3 = TDHelpers.ConsolidateByTotalCount(array, 1);
-            Assert.IsTrue(merge3.Length == 1);
+            Assert.That(merge3.Length == 1);
 
-            Assert.IsTrue(merge3[0].open == 0);
-            Assert.IsTrue(merge3[0].high == 8);
-            Assert.IsTrue(merge3[0].low == 0);
-            Assert.IsTrue(merge3[0].close == 8);
-            Assert.IsTrue(merge3[0].volume == 36);
+            Assert.That(merge3[0].open == 0);
+            Assert.That(merge3[0].high == 8);
+            Assert.That(merge3[0].low == 0);
+            Assert.That(merge3[0].close == 8);
+            Assert.That(merge3[0].volume == 36);
         }
 
 
@@ -90,45 +90,45 @@ namespace TDAmeritrade.Tests
                 array[i] = new TDPriceCandle { close = i, high = i, low = i, open = i, volume = i, datetime = i };
             }
             var merge1 = TDHelpers.ConsolidateByPeriodCount(array, 3);
-            Assert.IsTrue(merge1.Length == 3);
+            Assert.That(merge1.Length == 3);
             var merge2 = TDHelpers.ConsolidateByPeriodCount(array, 5);
-            Assert.IsTrue(merge2.Length == 2);
+            Assert.That(merge2.Length == 2);
             var merge3= TDHelpers.ConsolidateByPeriodCount(array, 9);
-            Assert.IsTrue(merge3.Length == 1);
+            Assert.That(merge3.Length == 1);
 
-            Assert.IsTrue(merge3[0].open == 0);
-            Assert.IsTrue(merge3[0].high == 8);
-            Assert.IsTrue(merge3[0].low == 0);
-            Assert.IsTrue(merge3[0].close == 8);
-            Assert.IsTrue(merge3[0].volume == 36);
+            Assert.That(merge3[0].open == 0);
+            Assert.That(merge3[0].high == 8);
+            Assert.That(merge3[0].low == 0);
+            Assert.That(merge3[0].close == 8);
+            Assert.That(merge3[0].volume == 36);
         }
 
         [Test]
         public async Task TestTDQuoteClient_Equity()
         {
             var data = await client.GetQuote_Equity("MSFT");
-            Assert.IsTrue(data.symbol == "MSFT");
+            Assert.That(data.symbol == "MSFT");
         }
 
         [Test]
         public async Task TestTDQuoteClient_Index()
         {
             var data = await client.GetQuote_Index("$SPX.X");
-            Assert.IsTrue(data.symbol == "$SPX.X");
+            Assert.That(data.symbol == "$SPX.X");
         }
 
         [Test]
         public async Task TestTDQuoteClient_Future()
         {
             var data = await client.GetQuote_Future("/ES");
-            Assert.IsTrue(data.symbol == "ES");
+            Assert.That(data.symbol == "ES");
         }
 
         [Test]
         public async Task TestTDQuoteClient_Option()
         {
             var data = await client.GetQuote_Option("SPY_231215C500");
-            Assert.IsTrue(data.symbol == "SPY_231215C500");
+            Assert.That(data.symbol == "SPY_231215C500");
         }
 
         [Test]
@@ -143,7 +143,7 @@ namespace TDAmeritrade.Tests
                 periodType = TDPriceHistoryRequest.PeriodTypes.day,
                 period = 5,
             });
-            Assert.IsTrue(data.Length > 0);
+            Assert.That(data.Length > 0);
         }
 
         [Test]
@@ -157,14 +157,14 @@ namespace TDAmeritrade.Tests
                 periodType = TDPriceHistoryRequest.PeriodTypes.day,
                 period = 5,
             });
-            Assert.IsTrue(data.Length > 0);
+            Assert.That(data.Length > 0);
         }
 
         [Test]
         public async Task TestTDPrincipalClient()
         {
             var data = await client.GetPrincipals(TDPrincipalsFields.preferences, TDPrincipalsFields.streamerConnectionInfo, TDPrincipalsFields.streamerSubscriptionKeys);
-            Assert.IsTrue(!string.IsNullOrEmpty(data.accessLevel));
+            Assert.That(!string.IsNullOrEmpty(data.accessLevel));
         }
 
         [Test]
@@ -197,7 +197,7 @@ namespace TDAmeritrade.Tests
                 await socket.SubscribeBook(symbol, TDBookOptions.LISTED_BOOK);
                 await socket.SubscribeBook(symbol, TDBookOptions.NASDAQ_BOOK);
                 await Task.Delay(1000);
-                Assert.IsTrue(socket.IsConnected);
+                Assert.That(socket.IsConnected);
                 await socket.Disconnect();
             }
         }
@@ -221,8 +221,8 @@ namespace TDAmeritrade.Tests
                 await socket.SubscribeChart(symbol, TDChartSubs.CHART_FUTURES);
                 await socket.SubscribeTimeSale(symbol, TDTimeSaleServices.TIMESALE_FUTURES);
                 await Task.Delay(1000);
-                Assert.IsTrue(socket.IsConnected);
-                Assert.IsTrue(socket.IsConnected);
+                Assert.That(socket.IsConnected);
+                Assert.That(socket.IsConnected);
                 await socket.Disconnect();
             }
         }
@@ -259,7 +259,7 @@ namespace TDAmeritrade.Tests
             reader.Parse("{\"data\":[{ \"service\":\"TIMESALE_EQUITY\", \"timestamp\":1620331268678,\"command\":\"SUBS\",\"content\":[{ \"seq\":206718,\"key\":\"QQQ\",\"1\":1620331267917,\"2\":331.57,\"3\":57.0,\"4\":220028}]}]}");
             reader.Parse("{\"data\":[{ \"service\":\"CHART_FUTURES\", \"timestamp\":1620348064760,\"command\":\"SUBS\",\"content\":[{ \"seq\":522,\"key\":\"/NQ\",\"1\":1620348000000,\"2\":13633.75,\"3\":13634.25,\"4\":13633.0,\"5\":13633.5,\"6\":38.0}]}]}");
             reader.Parse("{\"data\":[{ \"service\":\"NASDAQ_BOOK\", \"timestamp\":1620658957880,\"command\":\"SUBS\", \"content\": [{\"key\":\"QQQ\",\"1\":1620658957722,\"2\": [{\"0\":328.47,\"1\":535,\"2\":3,\"3\":[{\"0\":\"NSDQ\",\"1\":335,\"2\":36155235}, {\"0\":\"phlx\",\"1\":100,\"2\":36157556},{\"0\":\"arcx\",\"1\":100,\"2\":36157656}]}, {\"0\":328.46,\"1\":2800,\"2\":3,\"3\":[{\"0\":\"batx\",\"1\":1000,\"2\":36157696}, {\"0\":\"nyse\",\"1\":1000,\"2\":36157697},{\"0\":\"edgx\",\"1\":800,\"2\":36157694}]}, {\"0\":328.45,\"1\":200,\"2\":2,\"3\":[{\"0\":\"MEMX\",\"1\":100,\"2\":36157694}, {\"0\":\"bosx\",\"1\":100,\"2\":36157696}]},{\"0\":328.44,\"1\":1200,\"2\":4,\"3\":[{\"0\":\"cinn\",\"1\":300,\"2\":36157339},{\"0\":\"edga\",\"1\":300,\"2\":36157555}, {\"0\":\"baty\",\"1\":300,\"2\":36157592},{\"0\":\"MIAX\",\"1\":300,\"2\":36157694}]}, {\"0\":328.42,\"1\":200,\"2\":1,\"3\":[{\"0\":\"iexg\",\"1\":200,\"2\":36157695}]}, {\"0\":327.34,\"1\":100,\"2\":1,\"3\":[{\"0\":\"mwse\",\"1\":100,\"2\":36126129}]}, {\"0\":326.77,\"1\":100,\"2\":1,\"3\":[{\"0\":\"amex\",\"1\":100,\"2\":36146149}]}], \"3\":[{\"0\":328.48,\"1\":1200,\"2\":4,\"3\":[{\"0\":\"NSDQ\",\"1\":300,\"2\":36157695}, {\"0\":\"phlx\",\"1\":300,\"2\":36157695},{\"0\":\"arcx\",\"1\":300,\"2\":36157696}, {\"0\":\"nyse\",\"1\":300,\"2\":36157696}]},{\"0\":328.49,\"1\":2800,\"2\":4,\"3\":[{\"0\":\"batx\",\"1\":1400,\"2\":36157337}, {\"0\":\"edgx\",\"1\":900,\"2\":36157695},{\"0\":\"MIAX\",\"1\":300,\"2\":36157694},{\"0\":\"MEMX\",\"1\":200,\"2\":36157694}]}, {\"0\":328.5,\"1\":300,\"2\":1,\"3\":[{\"0\":\"bosx\",\"1\":300,\"2\":36157695}]}, {\"0\":328.51,\"1\":1500,\"2\":3,\"3\":[{\"0\":\"baty\",\"1\":600,\"2\":36157337},{\"0\":\"edga\",\"1\":600,\"2\":36157695}, {\"0\":\"cinn\",\"1\":300,\"2\":36157656}]},{\"0\":328.59,\"1\":200,\"2\":1,\"3\":[{\"0\":\"iexg\",\"1\":200,\"2\":36157696}]}, {\"0\":329.55,\"1\":100,\"2\":1,\"3\":[{\"0\":\"mwse\",\"1\":100,\"2\":35431559}]}, {\"0\":336.64,\"1\":200,\"2\":1,\"3\":[{\"0\":\"GSCO\",\"1\":200,\"2\":30661019}]} ]}]}]}");
-            Assert.IsTrue(counter == 0);
+            Assert.That(counter == 0);
         }
     }
 }
